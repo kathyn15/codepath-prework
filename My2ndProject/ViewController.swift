6961042f8b82266be80fb972ba54274147b76a57
kathyn15 <nguyenkathy531@gmail.com>
2023-11-15 08:55:14 -0600

    Added prework files to repository

---
+++ My2ndProject/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  My2ndProject
+//
+//  Created by Kathy Nguyen on 11/15/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+    }
+    
+    
+    func changeColor() -> UIColor{
+
+            let red = CGFloat.random(in: 0...1)
+            let green = CGFloat.random(in: 0...1)
+            let blue = CGFloat.random(in: 0...1)
+
+            return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+        }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
